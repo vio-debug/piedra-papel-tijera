868854edb63de3e145dbf1c18fd83a7059d96499
Vio <vioeasd@gmail.com>
2024-11-13 21:41:53 +0100

    ahora si sirve

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-alert("A");
-var jugador1 = prompt('Jugador 1 = Escoge entre : "Piedra/Papel/Tijeras"');
-var jugador2 = prompt('Jugador 2 = Escoge entre : "Piedra/Papel/Tijeras"');
-
-
+var jugador1 = prompt('Jugador 1 = Escoge entre : "Piedra🪨/Papel🧻/Tijeras✂️"');
+var jugador2 = prompt('Jugador 2 = Escoge entre : "Piedra🪨/Papel🧻/Tijeras✂️"');
 
 
 if((jugador1 !== "Piedra" && jugador1 !== "Papel" && jugador1 !== "Tijeras") || (jugador2 !== "Piedra" && jugador2 !== "Papel" && jugador2 !== "Tijeras")){
 
-    alert("ERROR!");
+    alert("Escríbelo bien loser👎");
 
 } else if (jugador1=="Piedra" && jugador2=="Tijeras" || jugador1=="Tijeras" && jugador2=="Papel" || jugador1=="Papel" && jugador2=="Piedra"){
    
-    alert("¡Jugador 1 gana!");
+    alert("🦕¡Jugador 1 gana!🤖");
 
 } else if (jugador1==jugador2){
 
-    alert("¡Empate!");
+    alert("🤝¡Empate!🤝");
 
 } else {
     
-    alert("¡Jugador 2 gana!");
+    alert("🦖¡Jugador 2 gana!🧟");
 
-    }
 }
